548a8a0b800c96696e220474f72d02e019b07321
Kauan Reis <kauanreisgomes123@gmail.com>
2022-10-20 16:26:28 -0300

    Build: Created interface GF_cadastro_usuario

--- src/main/java/com/decattech/controller/GF_home.java
+++ src/main/java/com/decattech/controller/GF_home.java
@@ -13,15 +13,15 @@ public class GF_home {
 
     @FXML private ImageView logo_emp;
     
-    @FXML private Label menu, menuback,lbuser;
+    @FXML private Label menu, lbuser,lbversion;
 
     @FXML private VBox vbmenu,vbmenu_children;
     
-    @FXML private MenuItem propriedades,liberar_telas,logout,relatorios, cadastro_setor,menu_login,cadastro_usuarios;
+    @FXML private MenuItem propriedades,liberar_telas,logout,relatorios, cadastro_setor,cadastro_usuarios;
 
-    @FXML private MenuButton menu_cadastros, menu_relatorio,menu_config,menu_processo;
+    @FXML private MenuButton menu_cadastros, menu_relatorio,menu_config,menu_processo,menu_login;
 
-    void initialize(){
+    @FXML void initialize(){
         Load();
     }
 
@@ -30,37 +30,51 @@ public class GF_home {
     }
     
     private void AnimationMenu(){
-		vbmenu_children.setTranslateX(-200);
-		menu.setOnMouseClicked(e->{
+		vbmenu.setTranslateX(-500);
+		menu.setOnMouseEntered(e->{
 			TranslateTransition slide = new TranslateTransition();
-            slide.setDuration(Duration.seconds(0.5));
-            slide.setNode(vbmenu_children);
+            slide.setDuration(Duration.seconds(0.3));
+            slide.setNode(vbmenu);
 
             slide.setToX(0);
             slide.play();
-            vbmenu_children.setTranslateX(-200);
-            slide.setOnFinished(e1->{
-                menu.setVisible(false);
-                menuback.setVisible(true);
-            });
+            vbmenu.setTranslateX(-500);
+           
 		});
 
-		menuback.setOnMouseClicked(e->{
-			
-            TranslateTransition slide = new TranslateTransition();
-            slide.setDuration(Duration.seconds(0.5));
-            slide.setNode(vbmenu_children);
+		menu.setOnMouseClicked(e->{
+			if(vbmenu.getTranslateX() != -500){
+                TranslateTransition slide = new TranslateTransition();
+                slide.setDuration(Duration.seconds(0.3));
+                slide.setNode(vbmenu);
 
-            slide.setToX(-200);
-            slide.play();
+                slide.setToX(-500);
+                slide.play();
 
-            vbmenu_children.setTranslateX(0);
+                vbmenu.setTranslateX(0);
+            }else{
+                TranslateTransition slide = new TranslateTransition();
+                slide.setDuration(Duration.seconds(0.3));
+                slide.setNode(vbmenu);
 
-            slide.setOnFinished(e1->{
-                menu.setVisible(true);
-                menuback.setVisible(false);
-            });
+                slide.setToX(0);
+                slide.play();
+                vbmenu.setTranslateX(-500);
+            }
 		});
+
+        MenuButton[] p = {menu_cadastros, menu_relatorio,menu_config,menu_processo,menu_login};
+        for (int i = 0; i < p.length; i++) {
+            MenuButton mb = p[i];
+            mb.setOnMouseEntered(e->{
+                mb.show();
+                for (int j = 0; j < p.length; j++) {
+                    if(!p[j].equals(mb)){
+                        p[j].hide();
+                    }
+                }
+            });
+        }
 	}
 
 }
